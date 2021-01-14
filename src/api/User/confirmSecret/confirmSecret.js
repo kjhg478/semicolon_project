2ff6bd739a643d9a46eef20bb4738f0300c5000b
JHK <31474272+kjhg478@users.noreply.github.com>
2021-01-14 18:51:38 +0900

    SemiColon Project

---
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -0,0 +1,18 @@
+import { prisma } from "../../../../generated/prisma-client";
+import { generateToken } from "../../../utils";
+
+export default {
+    Mutation: {
+        confirmSecret: async (_, args, { request }) => {
+            console.log(request.user);
+            
+            const { email, secret } = args;
+            const user = await prisma.user({ email });
+            if (user.loginSecret === secret) {
+                return generateToken(user.id);
+            } else {
+                throw Error("입력한 값이 맞지 않습니다. 값을 다시 입력해주세요");
+            }
+        }
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
-require("dotenv").config()
+import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import "./passport";
+import { authenticateJwt } from "./passport"
 
 // 그래프 큐엘 서버는 express 서버가 내장되어 있음
 
-const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const PORT = process.env.PORT;
 
+const server = new GraphQLServer({
+    schema,
+    context: ({ request }) => ({ request })
+});
+// context resover와 정보를 공유할 때 사용함
 server.express.use(logger("dev"));
+server.express.use(authenticateJwt);
 
 server.start({ port: PORT }, () => console.log(`✔ Server running on http://localhost:${PORT}`)
 );
